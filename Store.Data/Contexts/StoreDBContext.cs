8331c43d6095dde9830da9de0a097b2beb2cc66f
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-09-24 14:54:12 +0300

    .

---
+++ Store.Data/Contexts/StoreDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Data.Contexts
+{
+    public class StoreDBContext : DbContext
+    {
+        public StoreDBContext(DbContextOptions<StoreDBContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreDBContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
